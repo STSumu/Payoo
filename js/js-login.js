bfe417ff9f131f058aaa26f6e243103b9d0ba583
STSumu <sabihatahsinsumu@gmail.com>
2024-09-24 19:52:12 +0600

    Added login page and homepage

---
+++ js/js-login.js
@@ -0,0 +1,18 @@
+console.log("button clicking file added");
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber=document.getElementById('phn-number').value;
+    const pinNumber=document.getElementById('pin').value;
+    console.log(phoneNumber,pinNumber);
+    console.log("button is clicked");
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log("you are logged in");
+        window.location.href="home.html"
+    }
+    else{
+        alert('Wrong phone number or pin');
+    }
+    phoneNumber='';
+    pinNumber='';
+});
